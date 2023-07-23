5e5c04d10bbbca42bacbe3278b57bdae2044d654
Madhu <madhuravius@protonmail.com>
2023-07-23 12:52:09 -0400

    chore: added hcl to treesitter

--- init.lua
+++ init.lua
@@ -264,7 +264,8 @@ pcall(require('telescope').load_extension, 'neoclip')
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'elixir', 'go', 'lua', "nix", 'python', 'ruby', 'rust', 'tsx', 'typescript', 'vimdoc',
+  ensure_installed = { 'c', 'cpp', 'elixir', 'go', 'hcl', 'lua', "nix", 'python', 'ruby', 'rust', 'tsx', 'typescript',
+    'vimdoc',
     'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
