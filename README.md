b24818a7fa195621612c2a58a5e814d1edec17f5
Madhusudan Ravi <madhuravius@protonmail.com>
2023-10-04 15:26:57 -0400

    chore: updated docs/minor tidy

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 
 Initially based off: https://github.com/nvim-lua/kickstart.nvim
 
-To install: `git clone git@git.team-z.dev:madhuravius/nvim.git ~/.config/nvim`
+To install: 
+
+`git clone git@git.team-z.dev:madhuravius/nvim.git ~/.config/nvim`
+`git clone git@github.com:madhuravius/nvim.git ~/.config/nvim`
--- init.lua
+++ init.lua
@@ -1,40 +1,3 @@
---[[
-
-=====================================================================
-==================== READ THIS BEFORE CONTINUING ====================
-=====================================================================
-
-Kickstart.nvim is *not* a distribution.
-
-Kickstart.nvim is a template for your own configuration.
-  The goal is that you can read every line of code, top-to-bottom, understand
-  what your configuration is doing, and modify it to suit your needs.
-
-  Once you've done that, you should start exploring, configuring and tinkering to
-  explore Neovim!
-
-  If you don't know anything about Lua, I recommend taking some time to read through
-  a guide. One possible example:
-  - https://learnxinyminutes.com/docs/lua/
-
-  And then you can explore or search through `:help lua-guide`
-
-
-Kickstart Guide:
-
-I have left several `:help X` comments throughout the init.lua
-You should run that command and read that help section for more information.
-
-In addition, I have some `NOTE:` items throughout the file.
-These are for you, the reader to help understand what is happening. Feel free to delete
-them once you know what you're doing, but they should serve as a guide for when you
-are first encountering a few different constructs in your nvim config.
-
-I hope you enjoy your Neovim journey,
-- TJ
-
-P.S. You can delete this when you're done too. It's your config now :)
---]]
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
@@ -175,17 +138,6 @@ require('lazy').setup({
     build = ':TSUpdate',
   },
 
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
   { import = 'custom.plugins' },
 }, {})
 
@@ -274,7 +226,20 @@ require('nvim-treesitter.configs').setup {
   modules = {},
   sync_install = false,
   ignore_install = {},
-  ensure_installed = { 'astro', 'c', 'cpp', 'elixir', 'go', 'hcl', 'lua', "nix", 'python', 'ruby', 'rust', 'tsx',
+  ensure_installed = {
+    'astro',
+    'c',
+    'cpp',
+    'elixir',
+    'go',
+    'hcl',
+    'lua',
+    "nix",
+    'proto',
+    'python',
+    'ruby',
+    'rust',
+    'tsx',
     'typescript',
     'vimdoc',
     'vim' },
@@ -418,6 +383,7 @@ local servers = {
 -- used for mason ensure installation of non-lsps
 local tools = {
   "black",
+  "buf",
   "eslint_d",
   "isort",
   "prettier",
