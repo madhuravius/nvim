90813f1df261aeaafdec9d533b12a6122167a9a8
Madhusudan Ravi <madhuravius@protonmail.com>
2025-03-15 09:20:58 -0400

    chore: temporarily disable noice, slow

--- lua/custom/plugins/layout.lua
+++ lua/custom/plugins/layout.lua
@@ -15,72 +15,73 @@ return {
       }
     end,
   },
-  {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-    },
-    config = function()
-      require('noice').setup {
-        routes = {
-          {
-            filter = {
-              event = 'msg_show',
-              kind = '',
-              find = 'written',
-            },
-            opts = { skip = true },
-          },
-        },
-        lsp = {
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
-        },
-        views = {
-          cmdline_popup = {
-            border = {
-              style = 'rounded',
-              padding = { 1, 1 },
-            },
-            filter_options = {},
-            win_options = {
-              winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
-            },
-          },
-          popupmenu = {
-            relative = 'editor',
-            position = {
-              row = 8,
-              col = '50%',
-            },
-            size = {
-              width = 60,
-              height = 10,
-            },
-            border = {
-              style = 'rounded',
-              padding = { 0, 1 },
-            },
-            win_options = {
-              winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
-            },
-          },
-        },
-      }
-    end,
-  },
+  -- commenting Noice for now, while nice poor performance
+  -- {
+  --   'folke/noice.nvim',
+  --   event = 'VeryLazy',
+  --   dependencies = {
+  --     'MunifTanjim/nui.nvim',
+  --     'rcarriga/nvim-notify',
+  --   },
+  --   config = function()
+  --     require('noice').setup {
+  --       routes = {
+  --         {
+  --           filter = {
+  --             event = 'msg_show',
+  --             kind = '',
+  --             find = 'written',
+  --           },
+  --           opts = { skip = true },
+  --         },
+  --       },
+  --       lsp = {
+  --         override = {
+  --           ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+  --           ['vim.lsp.util.stylize_markdown'] = true,
+  --           ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
+  --         },
+  --       },
+  --       presets = {
+  --         bottom_search = true, -- use a classic bottom cmdline for search
+  --         command_palette = true, -- position the cmdline and popupmenu together
+  --         long_message_to_split = true, -- long messages will be sent to a split
+  --         inc_rename = false, -- enables an input dialog for inc-rename.nvim
+  --         lsp_doc_border = true, -- add a border to hover docs and signature help
+  --       },
+  --       views = {
+  --         cmdline_popup = {
+  --           border = {
+  --             style = 'rounded',
+  --             padding = { 1, 1 },
+  --           },
+  --           filter_options = {},
+  --           win_options = {
+  --             winhighlight = 'NormalFloat:NormalFloat,FloatBorder:FloatBorder',
+  --           },
+  --         },
+  --         popupmenu = {
+  --           relative = 'editor',
+  --           position = {
+  --             row = 8,
+  --             col = '50%',
+  --           },
+  --           size = {
+  --             width = 60,
+  --             height = 10,
+  --           },
+  --           border = {
+  --             style = 'rounded',
+  --             padding = { 0, 1 },
+  --           },
+  --           win_options = {
+  --             winhighlight = { Normal = 'Normal', FloatBorder = 'DiagnosticInfo' },
+  --           },
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
   {
     'nvim-neo-tree/neo-tree.nvim',
     version = '*',
