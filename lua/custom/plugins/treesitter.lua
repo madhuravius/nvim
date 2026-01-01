91d3e29ad27e903e49df9e150793787a5883766e
Madhusudan Ravi <madhuravius@protonmail.com>
2025-12-31 23:12:52 -0500

    chore: dropped unused textobjects

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -2,9 +2,6 @@ return {
   { 'jlcrochet/vim-crystal', event = 'VeryLazy' },
   {
     'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
     build = ':TSUpdate',
     lazy = false,
   },
